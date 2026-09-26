f235020528156cf25d57a167ab99b23cd83197f4
jbds <johnbertdelossantos480@gmail.com>
2026-09-26 17:45:38 +0800

    Scheduling Backend: Token Authentication API

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ScheduleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,4 +25,16 @@ Route::get('/users', function() {
     return response()->json([
         'data' => \App\Models\User::all(),
     ]);
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return response()->json([
+            'data' => $request->user(),
+        ]);
+    });
 });
